72ef996de781375e0d814daff6b599f5e38fb84e
iamakbarsha0 <akbar.sha@concertidc.com>
2025-09-25 17:02:44 +0530

    refactor the Utils file: export; add normalize_list, json_body_from_raw, ok, error

--- lib/RebirthAPI/Utils.pm
+++ lib/RebirthAPI/Utils.pm
@@ -3,20 +3,25 @@ package RebirthAPI::Utils;
 use strict;
 use warnings;
 use Exporter 'import';
+use JSON qw(decode_json);
 
-our @EXPORT_OK = qw(normalize_bson);
+our @EXPORT_OK = qw(
+    normalize_bson
+    normalize_list
+    json_body_from_raw
+    ok
+    error
+);
 
+# Convert BSON::OID to string inside a doc
 sub normalize_bson {
     my ($doc) = @_;
-    # If it's undefined or a plain scalar, just return as-is
     return $doc unless defined $doc;
 
-    # If the entire value is a BSON::OID, stringify it
     if (ref($doc) && eval { $doc->isa('BSON::OID') }) {
         return $doc->to_string;
     }
 
-    # If it's a HASH ref, possibly stringify the _id field
     if (ref($doc) eq 'HASH') {
         my $oid = $doc->{_id};
         if (defined $oid && ref($oid) && eval { $oid->can('to_string') }) {
@@ -25,8 +30,38 @@ sub normalize_bson {
         return $doc;
     }
 
-    # For ARRAY refs or other structures, return as-is for now
     return $doc;
+}
+
+# Normalize an array of docs
+sub normalize_list {
+    my ($list) = @_;
+    return [] unless defined $list && ref($list) eq 'ARRAY';
+    my @clean = map { normalize_bson($_) } @$list;
+    return \@clean;
+}
+
+# Pure JSON decode from raw string
+sub json_body_from_raw {
+    my ($raw) = @_;
+    my $data;
+    eval { $data = decode_json($raw // '{}') };
+    $data = {} if $@ || !defined $data || ref($data) ne 'HASH';
+    return $data;
+}
+
+# Success response helper
+sub ok {
+    my ($payload) = @_;
+    $payload ||= {};
+    return { success => 1, %{$payload} };
+}
+
+# Error response helper
+sub error {
+    my ($message) = @_;
+    $message ||= 'Internal Server Error';
+    return { success => 0, error => $message };
 }
 
 1;
